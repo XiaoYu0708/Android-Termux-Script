4134e6fb5695b2fabb1d77dfa41603e2e4579424
XiaoYu <68182187+XiaoYu0708@users.noreply.github.com>
2025-05-09 16:57:27 +0800

    Update debian_fast_apt.sh

--- debian_fast_apt.sh
+++ debian_fast_apt.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-#編輯映射站列表
-vim /etc/apt/sources.list
-
 #將網址全部替換成台灣國網中心的網址（需注意版本代號，目前是bookworm）：
-deb http://opensource.nchc.org.tw/debian/ bookworm main contrib non-free
-deb-src http://opensource.nchc.org.tw/debian/ bookworm main contrib non-free
-deb http://opensource.nchc.org.tw/debian/ bookworm-updates main
-deb-src http://opensource.nchc.org.tw/debian/ bookworm-updates main
-deb http://security.debian.org/debian-security bookworm/updates main contrib non-free
-deb http://opensource.nchc.org.tw/debian bookworm-backports main
+echo "deb http://opensource.nchc.org.tw/debian/ bookworm main contrib non-free" >> /etc/apt/sources.list
+echo "deb-src http://opensource.nchc.org.tw/debian/ bookworm main contrib non-free" > /etc/apt/sources.list
+echo "deb http://opensource.nchc.org.tw/debian/ bookworm-updates main" > /etc/apt/sources.list
+echo "deb-src http://opensource.nchc.org.tw/debian/ bookworm-updates main" > /etc/apt/sources.list
+echo "deb http://security.debian.org/debian-security bookworm/updates main contrib non-free" > /etc/apt/sources.list
+echo "deb http://opensource.nchc.org.tw/debian bookworm-backports main" > /etc/apt/sources.list
 
 #更新套件列表
 apt update
