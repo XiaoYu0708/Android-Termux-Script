f627c53f963e6169b6694d9337b285f1d27f7b51
XiaoYu <68182187+XiaoYu0708@users.noreply.github.com>
2025-05-09 16:49:35 +0800

    Update install_debian.sh

--- install_debian.sh
+++ install_debian.sh
@@ -14,17 +14,3 @@ proot-distro login debian --user root --shared-tmp
 #登入後先安裝sudo、vim、Firefox瀏覽器
 apt update
 apt install sudo vim firefox-esr
-
-#編輯映射站列表
-vim /etc/apt/sources.list
-
-#將網址全部替換成台灣國網中心的網址（需注意版本代號，目前是bookworm）：
-deb http://opensource.nchc.org.tw/debian/ bookworm main contrib non-free
-deb-src http://opensource.nchc.org.tw/debian/ bookworm main contrib non-free
-deb http://opensource.nchc.org.tw/debian/ bookworm-updates main
-deb-src http://opensource.nchc.org.tw/debian/ bookworm-updates main
-deb http://security.debian.org/debian-security bookworm/updates main contrib non-free
-deb http://opensource.nchc.org.tw/debian bookworm-backports main
-
-#更新套件列表
-apt update
