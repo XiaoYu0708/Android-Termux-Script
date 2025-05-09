878a0109f45dc47706080989b7d854ef55735774
XiaoYu <68182187+XiaoYu0708@users.noreply.github.com>
2025-05-09 17:15:54 +0800

    Create install_conda.sh

---
+++ install_conda.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 安裝miniforge3
+apt update
+apt install wget python3 python3-pip vim
+
+alias python=/usr/bin/python3
+alias python3=/usr/bin/python3
+alias pip=/usr/bin/pip
+alias pip3=/usr/bin/pip
+
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-aarch64.sh
+bash Miniforge3-Linux-aarch64.sh
+exit
